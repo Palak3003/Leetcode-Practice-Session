7fc21d38d327de0da55f6aaa86cf60dfb621015f
Palak3003 <bhardwajpalak3003@gmail.com>
2026-04-23 09:46:36 +0530

    Day-2

---
+++ q6RemoveElement.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Q6Removeelement{
+
+    public static int removeElement(int[] nums, int val) {
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+        return j;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter value to remove: ");
+        int val = sc.nextInt();
+
+        int k = removeElement(nums, val);
+
+        System.out.println("Array after removing element:");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        System.out.println("\nNew length: " + k);
+
+    }
+}
